4f5309375ad5e92f2bbf14c21246321ad715423f
Eduardo Alvarez <90423330+d3val@users.noreply.github.com>
2024-10-20 09:40:26 -0600

    Ultimos cambios (#17)
    * Prefabs chnages
    
    * Game over
    
    * Main menu
    
    * Build

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
